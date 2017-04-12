eedcd1cce484d45e1919246929a5c53d69ecbe60
tjinjin <tjinjinprogram@gmail.com>
2017-04-12 23:56:21 +0900

    role

--- roles/wp.rb
+++ roles/wp.rb
@@ -1,4 +1,6 @@
+include_recipe 'selinux::permissive'
 include_recipe '../cookbooks/mysql'
 include_recipe '../cookbooks/nginx'
 include_recipe '../cookbooks/php'
 include_recipe '../cookbooks/wordpress'
+include_recipe '../cookbooks/vim'
