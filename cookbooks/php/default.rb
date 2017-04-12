f838b95f9f7cc805a9d57fdeb9091d6412007792
tjinjin <tjinjinprogram@gmail.com>
2017-04-12 23:56:00 +0900

    Add php

--- cookbooks/php/default.rb
+++ cookbooks/php/default.rb
@@ -2,9 +2,10 @@ package 'php'
 package 'php-mysql'
 package 'php-fpm'
 
-#remote_file '/etc/php-fpm.d/www.conf' do
-#  source 'files/www.conf'
-#end
+remote_file '/etc/php-fpm.d/www.conf' do
+  source 'files/www.conf'
+  notifies :restart,"service[php-fpm]"
+end
 
 service 'php-fpm' do
   action [:enable, :start]
